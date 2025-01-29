b1d1d5c2a1131db1a8cbea8cb1c277c97017fe34
Marie <marie@ha.si>
2025-01-29 11:42:12 +0100

    some improvements

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ struct Permutation {
     entropy: f32,
 
     /// The permutations derived from this permutation and "adopted" permutations
-    child_permutations: Option<Vec<Permutation>>,
+    child_permutations: Vec<Permutation>,
 }
 
 /// generate all possible permutations for the given lines. The count and total
@@ -24,15 +24,15 @@ fn shuffle_lines(
     min_blocksize: usize,
     count: &mut usize,
     total: usize,
-) -> Option<Vec<Permutation>> {
+) -> Vec<Permutation> {
+    let mut line_permutations: Vec<Permutation> = Vec::new();
+
     if blocksize == 1 || blocksize < min_blocksize {
-        return None;
+        return line_permutations;
     }
 
     //blocksize = (blocksize as f32 / 2.0).ceil() as usize;
     blocksize /= 2;
-
-    let mut line_permutations: Vec<Permutation> = Vec::new();
 
     let chunks = lines.into_iter().chunks(blocksize);
     let mut chunks_vec: Vec<Vec<usize>> = Vec::new();
@@ -68,7 +68,7 @@ fn shuffle_lines(
         });
     }
 
-    Some(line_permutations)
+    line_permutations
 }
 
 /// Makes sure that the given set of permutations is no longer than MAX_PARENT_PERMUTATIONS.
@@ -80,16 +80,20 @@ fn shuffle_lines(
 /// -> [1, 2, 3, 4], [5, 6, 7, 8], [9, 10]
 /// -> [1 children: [2, 3, 4]], [5 children: [6, 7, 8]], [9 children: [10]]
 /// ```
-fn optimize_permutations(permutations: &mut Vec<Permutation>, count: &mut usize) {
+fn optimize_permutations(
+    permutations: &mut Vec<Permutation>,
+    count: &mut usize,
+    total_perms: usize,
+) {
     // The value of this constant could also be used to determine a count of
     // jobs which simultaneously check all permutations.
     const MAX_PARENT_PERMUTATIONS: usize = 2;
 
-    *count += 1;
     eprint!(
-        "optimizing permutation set.......................: {}\r",
-        count
+        "optimizing permutation set.......................: {}/{}\r",
+        count, total_perms
     );
+    *count += 1;
 
     while permutations.len() > MAX_PARENT_PERMUTATIONS {
         let permutation_chunks = permutations.into_iter().chunks(MAX_PARENT_PERMUTATIONS);
@@ -108,14 +112,8 @@ fn optimize_permutations(permutations: &mut Vec<Permutation>, count: &mut usize)
             let mut parent = chunk[0].clone();
             chunk.remove(0);
 
-            if let None = parent.child_permutations {
-                parent.child_permutations = Some(Vec::new());
-            }
-
-            if let Some(children) = parent.child_permutations.as_mut() {
-                for child in chunk {
-                    children.push(child.clone());
-                }
+            for child in chunk {
+                parent.child_permutations.push(child.clone());
             }
 
             new_permutations.push(parent);
@@ -126,9 +124,7 @@ fn optimize_permutations(permutations: &mut Vec<Permutation>, count: &mut usize)
     }
 
     for permutation in permutations {
-        if let Some(child_permutations) = permutation.child_permutations.as_mut() {
-            optimize_permutations(child_permutations, count);
-        }
+        optimize_permutations(&mut permutation.child_permutations, count, total_perms);
     }
 }
 
@@ -156,13 +152,13 @@ fn calculate_estimated_permutation_count(
 
 /// Counts the total amount of generated permutation in a vector of boxed Permutation structs.
 fn count_permutations(permutations: &Vec<Permutation>) -> usize {
-    if permutations.is_empty() || permutations[0].child_permutations.as_ref().is_none() {
+    if permutations.is_empty() || permutations[0].child_permutations.is_empty() {
         return 0;
     }
 
     let mut prev: usize = 0;
     for permutation in permutations {
-        let cur = count_permutations(permutation.child_permutations.as_ref().unwrap());
+        let cur = count_permutations(&permutation.child_permutations);
         if cur > prev {
             prev = cur;
         }
@@ -171,19 +167,33 @@ fn count_permutations(permutations: &Vec<Permutation>) -> usize {
     1 + prev
 }
 
+fn make_mem_color(mem: f32) -> String {
+    let memory_color: String;
+
+    if mem >= 8.0 && mem < 64.0 {
+        memory_color = String::from("\x1b[33m");
+    } else if mem >= 64.0 {
+        memory_color = String::from("\x1b[31m");
+    } else {
+        memory_color = String::from("\x1b[32m");
+    }
+
+    memory_color
+}
+
 fn main() {
     let lines = "float i_event0 = 0;           // fade in
 float i_event5 = 2 * spsec;   // end of fade in
 float i_event28 = 22 * spsec;     // sixth scene (chains far away)
 float i_event28 = 22 * spsec;     // sixth scene (chains far away)
 float i_event28 = 22 * spsec;     // sixth scene (chains far away)
-float i_event27p5 = 20 * spsec;   // ship from other side
+float i_event80 = 36 * spsec; // second dreamy bright scene
 float i_event30 = 24 * spsec;     // start of first ship scene
 float i_event80 = 36 * spsec; // second dreamy bright scene"
         .to_string();
 
     // Minimal block size
-    let minbs: usize = 1;
+    let minbs: usize = 2;
 
     let lines = lines.split("\n").collect::<Vec<&str>>();
     let line_count: usize = lines.clone().len();
@@ -197,13 +207,16 @@ float i_event80 = 36 * spsec; // second dreamy bright scene"
 
     // The estimate is doubled since the optimisation step likely doubles the memory
     // usage in favour of more efficient searching
-    let estimated_memory_usage: f32 = estimated_permutation_count as f32
+    let estimated_memory_usage: f32 = (estimated_permutation_count as f32
         * (std::mem::size_of::<Permutation>() as f32
             + (line_count as f32 * std::mem::size_of::<usize>() as f32))
-        * 2.0;
+        * 2.0)
+        / (1024.0_f32.powf(3.0));
+
     eprintln!(
-        "estimated memory usage for all permutations (GiB): {}",
-        estimated_memory_usage / (1024.0_f32.powf(3.0))
+        "estimated memory usage for all permutations (GiB): \x1b[1m{}{}\x1b[0m",
+        make_mem_color(estimated_memory_usage),
+        estimated_memory_usage
     );
 
     let mut count: usize = 0;
@@ -214,8 +227,7 @@ float i_event80 = 36 * spsec; // second dreamy bright scene"
         minbs,
         &mut count,
         estimated_permutation_count,
-    )
-    .expect("should get some permutations");
+    );
 
     eprintln!("");
     eprintln!(
@@ -223,13 +235,15 @@ float i_event80 = 36 * spsec; // second dreamy bright scene"
         count
     );
 
-    count = 0;
-    optimize_permutations(&mut permutations, &mut count);
+    let mut opt_count = 0;
+    optimize_permutations(&mut permutations, &mut opt_count, count);
     eprintln!("");
 
+    let actual_memory_usage: f32 = permutations.deep_size_of() as f32 / (1024.0_f32.powf(3.0));
     eprintln!(
-        "\"actual\" memory usage for all permutations (GiB).: {}",
-        permutations.deep_size_of() as f32 / (1024.0_f32.powf(3.0))
+        "\"actual\" memory usage for all permutations (GiB).: \x1b[1m{}{}\x1b[0m",
+        make_mem_color(actual_memory_usage),
+        actual_memory_usage
     );
 
     eprintln!(
